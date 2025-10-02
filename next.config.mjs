d1ee256dd84d0fda90685f646396d7c3c9479a07
wahidalm <wahiduzalam@gmail.com>
2025-10-02 14:33:58 +0530

    Change config for production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Enables static exports
+  output: 'export',
+
+  // Your repository name (e.g., if your repo is 'my-next-app', this should be '/my-next-app')
+  basePath: '/your-repo-name', 
+
+  // Disables the default Next.js image optimization
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
